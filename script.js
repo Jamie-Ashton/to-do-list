7ffe0008cdc47ca78ab2d042408c8ae5ae77516f
Jamie-Ashton <110884625+Jamie-Ashton@users.noreply.github.com>
2024-10-16 10:55:33 +1300

    updating

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 // firebase configuration
-// init firebase
 
+
+  
+// init firebase
+const app = initializeApp(firebaseConfig);
+const database = firebase.database();
+const todoRef = firebase.database().ref('todos')
 
 //select DOM elements
 const addTodoButtom = document.getElementById('add-todo-button')
@@ -9,3 +14,41 @@ const todoList = document.getElementById('todo-list')
 const prioritySelect = document.getElementById('priority-select')
 const searchInpu = document.getElementById('search-input')
 const darkToggle = document.getElementById('dark-mode-toggle')
+
+
+//addEventListeners to add a todo item 
+
+addTodoButton.addEventListener('click', () => {
+  const todoText = todoInput.value
+  const priority = prioritySelect.value
+  if (todoText > 0) {
+    // create new reference in the database for new todo
+    const newTodoRef = todoRef.push()
+    const currentData = new Date().toLocalDateString()
+    
+    //set the new todo item's properties
+    newTodoRef.set({
+        text: todoText,
+        completed: false,
+        date: currentData,
+        priority: priority,
+        category: "General" //default category
+
+    })
+    //CLear the input field after adding a todo
+    todoInput = '';
+}
+})
+
+
+//Add keypress event to add todo with 'Enter' key
+todoInput.addEventListener('keypress', (e) => {
+    if(e.key === 'Enter') {
+        addTodoButton.click();
+    }
+});
+
+//Eventlistener to toggle dark mode
+darkToggle.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+})
