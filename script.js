84a7c671e26fd77d75aa9b0b85322c39a76a89c6
Jamie-Ashton <110884625+Jamie-Ashton@users.noreply.github.com>
2024-10-16 10:21:10 +1300

    adding js DOM elememts variables

---
+++ script.js
@@ -0,0 +1,11 @@
+// firebase configuration
+// init firebase
+
+
+//select DOM elements
+const addTodoButtom = document.getElementById('add-todo-button')
+const todoInput = document.getElementById('todo-input')
+const todoList = document.getElementById('todo-list')
+const prioritySelect = document.getElementById('priority-select')
+const searchInpu = document.getElementById('search-input')
+const darkToggle = document.getElementById('dark-mode-toggle')
